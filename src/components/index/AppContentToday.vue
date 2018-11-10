9f9cda1edb3a2ba5c29abbae1a8bca3a8889c14c
NegativeDearc <datingwithme@live.cn>
2018-11-10 12:55:00 +0800

    - small ui issue fix
    Signed-off-by: NegativeDearc <datingwithme@live.cn>

--- src/components/index/AppContentToday.vue
+++ src/components/index/AppContentToday.vue
@@ -1,14 +1,12 @@
 <template>
-  <v-content
-    app
-  >
-    <v-container fluid grid-list-lg>
+  <v-content app>
+    <v-container fluid grid-list-md>
       <v-layout>
-        <v-flex xs12 sm12 md6 offset-md3>
+        <v-flex xs12 sm12 md12>
           <div class="align-center display-1 font-weight-light grey--text"><span>Go GEMBA && Just Do It</span></div>
         </v-flex>
       </v-layout>
-      <v-layout>
+      <v-layout row wrap>
         <v-flex xs12 md6>
           <v-card>
             <v-progress-linear v-bind:value="$store.getters.GET_TODAY_FINISH_RATE" height="3" color="red"></v-progress-linear>
